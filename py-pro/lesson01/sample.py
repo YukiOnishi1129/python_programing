3bddd5c94cdd4032774b93e5f4fb1ccaa64b8542
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-03-17 23:29:06 +0900

    string function

---
+++ py-pro/lesson01/sample.py
@@ -0,0 +1,30 @@
+# num = 1
+# print(num)
+
+
+# num: int = 1
+# name: str = "1"
+
+# num = name
+
+# print(num, type(num))
+
+s = 'My name is Mike. Hi Mike.'
+# is_start = s.startswith('My')
+# print(is_start)
+
+# print(s.find('Mike'))
+
+# print(s.rfind('Mike'))
+
+# print(s.count('Mike'))
+
+# print(s.capitalize())
+
+# print(s.title())
+
+# print(s.upper())
+
+# print(s.lower())
+
+# print(s.replace('Mike', 'Nancy'))
