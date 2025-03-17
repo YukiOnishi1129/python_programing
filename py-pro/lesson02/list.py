2f3c4c379889eea41211681d980e6674c8dcd6b0
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-03-17 23:37:35 +0900

    list

---
+++ py-pro/lesson02/list.py
@@ -0,0 +1,40 @@
+# l = [1, 20, 4, 50, 2, 1, 2]
+# print(l)
+
+# print(l[0])
+# print(l[1])
+
+# print(l[-1])
+# print(l[-2])
+
+# print(l[0:2])
+# print(l[:2])
+
+# print(l[2:5])
+
+# print(len(l))
+# print(type(l))
+
+
+# n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# n.append(100)
+
+# # print(n)
+
+# n.insert(0, 200)
+# # print(n)
+
+# n.pop()
+# # print(n)
+
+# n.pop(0)
+# # print(n)
+
+# del n[0]
+# # print(n)
+
+# m = [1,2,2,2,3]
+# m.remove(2)
+
+# print(m)
+
