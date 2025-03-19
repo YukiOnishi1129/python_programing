659a93b29452dea84bb0f20e803abc8aa4d222f8
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-03-20 02:03:16 +0900

    validation error

--- fast-api/work_fastapi/pydantic/main.py
+++ fast-api/work_fastapi/pydantic/main.py
@@ -1,5 +1,5 @@
 from datetime  import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
 
 class Event(BaseModel):
     name: str = "未定"
@@ -8,11 +8,14 @@ class Event(BaseModel):
     
 external_data = {
 	"name": "FastAPI勉強会",
-	"start_datetime": "2021-09-01T12:00:00",
-	"participants": ["Alice", "Bob"]
+	"start_datetime": "abc",
+	"participants": ["Alice", "Bob", "Charlie"]
 }
 
-event = Event(**external_data)
-print("イベント名:", event.name, type(event.name))
-print("開始日時:", event.start_datetime, type(event.start_datetime))
-print("参加者:", event.participants, type(event.participants))
+try:
+	event = Event(**external_data)
+	print("イベント名:", event.name, type(event.name))
+	print("開始日時:", event.start_datetime, type(event.start_datetime))
+	print("参加者:", event.participants, type(event.participants))
+except ValidationError as e:
+    print("データのバリデーションエラーが発生しました：", e.errors())
